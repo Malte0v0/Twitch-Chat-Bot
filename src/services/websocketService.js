ad806e10145579c14b6777da932c3cd111058f9f
Malte0v0 <235309424+Malte0v0@users.noreply.github.com>
2025-06-26 19:59:37 +0200

    Fix prev commit

--- src/services/websocketService.js
+++ src/services/websocketService.js
@@ -31,7 +31,7 @@ export class WebSocketService {
         });
 
         websocketClient.on("close", (code, reason) => {
-            console.warn("Websocket closed:", code, JSON.parse(reason.toString()));
+            console.warn("Websocket closed:", code, reason.toString());
 
             let newUrl = websocketUrl;
 
@@ -59,7 +59,7 @@ export class WebSocketService {
     reconnect(websocketUrl=this._defaultWebsocketUrl) {
         clearInterval(this._keepaliveInterval);
         // Close the old websocketClient
-        if (this._websocketClient) {
+        if (this._websocketClient && this._websocketClient.readyState === WebSocket.OPEN) {
             this._websocketClient.terminate();
         }
         // Start a new one
